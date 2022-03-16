09ca2c022692e32387b22eaf46b3a4c45a26d1c8
Victor Alagwu <victoralagwu@gmail.com>
2022-03-16 12:32:26 +0100

    Array of Products

--- algoexpert/arrays/array-of-products/main.go
+++ algoexpert/arrays/array-of-products/main.go
@@ -1,27 +1,27 @@
 package main
 
+import "fmt"
 
 //Write a function that takes in a non-empty array of integers and returns an
 //array of the same length, where each element in the output array is equal to
 //the product of every other number in the input array.
 func main() {
-
+	arr := []int{5, 1, 4, 2}
+	fmt.Println("Array of Products: ", arrayOfProductsBruteForce(arr))
 }
 
 func arrayOfProductsBruteForce(array []int) []int {
 	// Write your code here.
 	arr := []int{}
 	for i := 0; i < len(array); i++ {
-		currentMultiple := 0
-		for j := 1; j < len(array) - 1; j++ {
-
-			if i + 1 == j {
-				break
+		currentMultiple := 1
+		for j := 0; j < len(array); j++ {
+			if i == j {
+				continue
 			}
-			currentMultiple *= i
-
+			currentMultiple *= array[j]
 		}
-		arr = append(arr, currentMultiple )
+		arr = append(arr, currentMultiple)
 	}
-	return nil
+	return arr
 }
