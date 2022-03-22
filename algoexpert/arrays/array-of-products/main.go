dfbbdd3fc5047e1809f58afdd189ae27f9ed5af7
Victor Alagwu <victoralagwu@gmail.com>
2022-03-22 09:02:20 +0100

    Array of Products

--- algoexpert/arrays/array-of-products/main.go
+++ algoexpert/arrays/array-of-products/main.go
@@ -7,7 +7,8 @@ import "fmt"
 //the product of every other number in the input array.
 func main() {
 	arr := []int{5, 1, 4, 2}
-	fmt.Println("Array of Products: ", arrayOfProductsBruteForce(arr))
+	fmt.Println("Array of Products (Brute Force): ", arrayOfProductsBruteForce(arr))
+	fmt.Println("Array of Products (Two Pointer): ", arrayOfProducts(arr))
 }
 
 func arrayOfProductsBruteForce(array []int) []int {
@@ -24,4 +25,38 @@ func arrayOfProductsBruteForce(array []int) []int {
 		arr = append(arr, currentMultiple)
 	}
 	return arr
+}
+
+func arrayOfProducts(array []int) []int {
+	lengthOfArr  := len(array)
+	currentIndex := 0
+	leftIndex := 0
+	rightIndex := lengthOfArr - 1
+	currentLeftProduct := 1
+	currentRightProduct := 1
+	leftProducts := make([]int, lengthOfArr)
+	rightProducts := make([]int, lengthOfArr)
+	products := make([]int, lengthOfArr)
+	for i := range leftProducts {
+		leftProducts[i] = 1
+		rightProducts[i] = 1
+		products[i] = 1
+	}
+
+	for leftIndex < lengthOfArr {
+		leftProducts[leftIndex] = currentLeftProduct
+		rightProducts[rightIndex] = currentRightProduct
+
+		currentLeftProduct *= array[leftIndex]
+		currentRightProduct *= array[rightIndex]
+
+		leftIndex++
+		rightIndex--
+	}
+	for currentIndex < len(leftProducts) {
+		products[currentIndex] = leftProducts[currentIndex] * rightProducts[currentIndex]
+		currentIndex++
+	}
+
+	return products
 }
